5702fe83746fdd46c4a74cf370b84acbf2880939
shivchangela1 <32464102+shivchangela1@users.noreply.github.com>
2017-12-05 14:28:39 -0800

    Update server.R

--- Shiny/server.R
+++ Shiny/server.R
@@ -1,9 +1,22 @@
 #server.R
 
 library(shiny)
+library(ggplot2)
+library(dplyr)
 
 # Define server logic
 shinyServer(function(input, output) {
-   
-  
+   my.server <- shinyServer(function(input, output) {
+    output$time.plot <- renderPlot({
+      date_time <- date_time %>% 
+        select(longitude, latitude, hour) %>%
+        filter(hour >= input$time) 
+
+      ggplot(data = date_time) +
+      geom_point(mapping = aes(x = longitude, y = latitude, color = hour)) + coord_map() +
+      labs(title = "Bike Used During Specified Hour and Above", xlab = "Longitude", ylab = "Latitude") +
+      theme(axis.title = element_text(size = 12, face = "bold")) +
+      theme(legend.text = element_text(size = 12, face = "bold")) +
+      theme(plot.title = element_text(size = 20, face = "bold"))
+    })
 })
