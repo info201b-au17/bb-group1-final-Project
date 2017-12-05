613b8d2475f65d3edcb295354e8852e5860dcffe
shivchangela1 <32464102+shivchangela1@users.noreply.github.com>
2017-12-05 14:29:44 -0800

    Update ui.R

--- Shiny/ui.R
+++ Shiny/ui.R
@@ -19,3 +19,24 @@ shinyUI(fluidPage(
     )
   )
 ))
+
+#SHIV'S CODE
+my.ui <- shinyUI(fluidPage(
+  
+  # Creates a title for the page
+  titlePanel("Last Time Bike was Used"),
+  
+  # Creates two widgets for user input
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("time",
+                  "Insert Hour:",
+                  00, 23, 00)
+    ),
+    
+    # Ouputs the plot to the website.
+    mainPanel(
+      plotOutput("time.plot")
+    )
+  )
+))
