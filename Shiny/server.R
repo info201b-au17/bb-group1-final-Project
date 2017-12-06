f6697464149be13e26c4f8d3c5e579251f445def
divanji2 <divanji2@uw.edu>
2017-12-06 01:49:43 -0800

    Reordered code to match question order

--- Shiny/server.R
+++ Shiny/server.R
@@ -9,6 +9,30 @@ library(tidyr)
 
 source("../DataIngest.R")
 
+#Question 1 - shannon's code, ploting on leaflet the location of the closest
+# 50 bikes to a station
+output$limeMap <- renderLeaflet({
+  Names<- c("Husky Stadium", "University Street",
+            "Capital Hill","International Station/Chinatown",
+            "Stadium","Sodo"
+  )
+  Lat<- c("47.6503", "47.607922", "47.620911",
+          "47.59850", "47.591961", "47.581309"
+  )
+  Long<- c("-122.3016", "-122.336058", "-122.320704",
+           "-122.328001", "-122.327315", "-122.327358"
+  )
+  buildingInfo<-data.frame(Names, Lat, Long)
+  building = buildingInfo %>% filter(Names == input$building) %>% select(Lat, Long)
+  pair<- c(as.character(building$Lat), as.character(building$Long))
+  points = getLimeBikeData(pair)
+  
+  leaflet() %>%
+    addTiles() %>%
+    addMarkers(data = points)
+})
+
+
 #Question 2 - Shiv's code
 data_time <- getTime()
 
@@ -62,8 +86,6 @@ my.server <- shinyServer(function(input, output) {
     }
     return(seattle.bikes)
   })
-  
-
   #output#2 - Creating Table with Bike & Bike Rack stats
   output$BikeRackTable <- renderTable({
     bikes <- transform(bikes, loc=paste(round(bikes$latitude, digits = 4), ", ", round(bikes$longitude, digits = 4)))
@@ -84,31 +106,5 @@ my.server <- shinyServer(function(input, output) {
     parked.bikes <- data.frame(Type, Count)
     return(parked.bikes)
   })
-  
-  
-
-  #Question 1 - shannon's code, ploting on leaflet the location of the closest
-  # 50 bikes to a station
-  output$limeMap <- renderLeaflet({
-    Names<- c("Husky Stadium", "University Street",
-              "Capital Hill","International Station/Chinatown",
-              "Stadium","Sodo"
-    )
-    Lat<- c("47.6503", "47.607922", "47.620911",
-            "47.59850", "47.591961", "47.581309"
-    )
-    Long<- c("-122.3016", "-122.336058", "-122.320704",
-             "-122.328001", "-122.327315", "-122.327358"
-    )
-    buildingInfo<-data.frame(Names, Lat, Long)
-    building = buildingInfo %>% filter(Names == input$building) %>% select(Lat, Long)
-    pair<- c(as.character(building$Lat), as.character(building$Long))
-    points = getLimeBikeData(pair)
-    
-    leaflet() %>%
-      addTiles() %>%
-      addMarkers(data = points)
-  })
-
 
 })
