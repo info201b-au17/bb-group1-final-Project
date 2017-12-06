6d977ea4b2a65005006a482c906b6715934248dc
shivchangela1 <32464102+shivchangela1@users.noreply.github.com>
2017-12-05 21:48:44 -0800

    Update server.R
    Changed variable names

--- Shiny/server.R
+++ Shiny/server.R
@@ -5,15 +5,19 @@ library(dplyr)
 library(ggmap)
 library(mapproj)
 
+source("DataIngest.R")
+
+data_time <- getTime()
+
 my.server <- shinyServer(function(input, output) {
   output$time.plot <- renderPlot({
-    date_time <- date_time %>% 
+    data_time <- data_time %>% 
       select(longitude, latitude, hour) %>%
       filter(hour >= input$time) 
     
     seattle.map <- get_map(location = "university of washington", maptype = "satellite", source = "google", zoom = 15)
     seattle.bikes <- ggmap(seattle.map) + 
-      geom_point(data = date_time, mapping = aes(x = longitude, y = latitude, color = hour)) +
+      geom_point(data = data_time, mapping = aes(x = longitude, y = latitude, color = hour)) +
       labs(title = "Bikes Used During Last Specified Hour", xlab = "Longitude", ylab = "Latitude") +
       theme(axis.title = element_text(size = 12, face = "bold")) +
       theme(legend.text = element_text(size = 12, face = "bold")) +
