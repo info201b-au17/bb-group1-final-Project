7de5e62fa0c713d127df6142281a7292c1fc5e28
Shannon Mallory <shannonmallory@Shannons-MacBook-Pro.local>
2017-12-05 23:48:19 -0800

    fixed the leaflet problem

--- Shiny/server.R
+++ Shiny/server.R
@@ -9,37 +9,11 @@ library(tidyr)
 
 source("../DataIngest.R")
 
-#Question 1 - shannon's code, ploting on leaflet the location of the closest
-# 50 bikes to a station
-output$limeMap <- renderLeaflet({
-  Names<- c("Husky Stadium", "University Street",
-            "Capital Hill","International Station/Chinatown",
-            "Stadium","Sodo"
-  )
-  Lat<- c("47.6503", "47.607922", "47.620911",
-          "47.59850", "47.591961", "47.581309"
-  )
-  Long<- c("-122.3016", "-122.336058", "-122.320704",
-           "-122.328001", "-122.327315", "-122.327358"
-  )
-  buildingInfo<-data.frame(Names, Lat, Long)
-  
-  building = buildingInfo %>% filter(Names == input$building) %>% select(Lat, Long)
-  print(building)
-  pair<- c(as.character(building$Lat), as.character(building$Long))
-  print(pair)
-  print(typeof(building$Lat))
-  points = getLimeBikeData(pair)
-  
-  leaflet() %>%
-    addTiles() %>%
-    addMarkers(data = points)
-})
-
 #Question 2 - Shiv's code
 data_time <- getTime()
 
 my.server <- shinyServer(function(input, output) {
+  #question 2
   output$time.plot <- renderPlot({
     data_time <- data_time %>% 
       select(longitude, latitude, hour) %>%
@@ -55,5 +29,27 @@ my.server <- shinyServer(function(input, output) {
     print(seattle.bikes)
   })
   
+  #Question 1 - shannon's code, ploting on leaflet the location of the closest
+  # 50 bikes to a station
+  output$limeMap <- renderLeaflet({
+    Names<- c("Husky Stadium", "University Street",
+              "Capital Hill","International Station/Chinatown",
+              "Stadium","Sodo"
+    )
+    Lat<- c("47.6503", "47.607922", "47.620911",
+            "47.59850", "47.591961", "47.581309"
+    )
+    Long<- c("-122.3016", "-122.336058", "-122.320704",
+             "-122.328001", "-122.327315", "-122.327358"
+    )
+    buildingInfo<-data.frame(Names, Lat, Long)
+    building = buildingInfo %>% filter(Names == input$building) %>% select(Lat, Long)
+    pair<- c(as.character(building$Lat), as.character(building$Long))
+    points = getLimeBikeData(pair)
+    
+    leaflet() %>%
+      addTiles() %>%
+      addMarkers(data = points)
+  })
 
 })
